8583c171670aa9d80bfea3d2655a43df96b2a6b1
abkrym5555 <momo542004@gmail.com>
2025-08-06 18:22:25 +0300

    add part js to nav bar

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+const openNav = document.querySelector("#opennav");
+const closeNav = document.querySelector("#close");
+const mobilenav = document.querySelector("#mobilenav");
+const labnav = document.querySelector("#labnav");
+const smNavLists = mobilenav.querySelectorAll("li");
+const bgNavLists = labnav.querySelectorAll("li");
+
+openNav.addEventListener("click", (e) => {
+  mobilenav.classList.remove("hiddennav");
+});
+
+closeNav.addEventListener("click", () => {
+  mobilenav.classList.add("hiddennav");
+});
+
+smNavLists.forEach((lis) => {
+  lis.addEventListener("click", (e) => {
+    smNavLists.forEach((li) => li.classList.remove("activesm"));
+    lis.classList.add("activesm");
+  });
+});
+
+bgNavLists.forEach((lis) => {
+  lis.addEventListener("click", (e) => {
+    bgNavLists.forEach((li) => li.classList.remove("activebg"));
+    lis.classList.add("activebg");
+  });
+});
